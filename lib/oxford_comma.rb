c04f3d5a0f506a99ae7fd15cba00706b2aa2ab59
Jobsidney <jobsidney67@gmail.com>
2022-08-17 20:28:19 +0300

    DOne with assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+    if array.length==1
+        array.join(" ")
+    elsif array.length==2
+        changed=array[-1]
+        neww=array
+        neww[-1]="and #{changed}"
+        neww.join(" ")
+    elsif array.length>2
+        changed=array[-1]
+        neww=array
+        neww[-1]="and #{changed}"
+        neww.join(", ")
+    else
+       
+    end
 end
+
+p oxford_comma(["fiddleheads","kohlrabi"])
